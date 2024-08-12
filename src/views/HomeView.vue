35f92a23b54528916a8584fb1c857d010de2db7f
Patrick Schwendemann <patrick.schwendemann@bamero.de>
2024-08-12 19:26:59 +0200

    Update distance and position styling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,14 +7,17 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <MainHeader headline="" backgroundColor="white" />
   <div class="content">
-    <h1>Herzlich Willkommen bei Waldmann!</h1>
-    <p>Checken Sie bitte hier ein oder aus.</p>
-    <p>Sollten Sie Fragen haben, können Sie sich jederzeit am Empfang melden.</p>
-
-    <nav>
-      <CustomButton text="CHECK OUT" backgroundColor="white" color="black" route="/" />
-      <CustomButton text="CHECK IN" backgroundColor="black" color="white" route="/auth" />
-    </nav>
+    <div class="home-container">
+      <h1>Herzlich Willkommen bei Waldmann!</h1>
+      <p>
+        Checken Sie bitte hier ein oder aus.<br />
+        Sollten Sie Fragen haben, können Sie sich jederzeit am Empfang melden.
+      </p>
+      <nav class="button-row">
+        <CustomButton text="CHECK OUT" backgroundColor="white" color="black" route="/" />
+        <CustomButton text="CHECK IN" backgroundColor="black" color="white" route="/auth" />
+      </nav>
+    </div>
   </div>
 </template>
 
@@ -22,5 +25,21 @@ import { RouterLink, RouterView } from 'vue-router'
 h1,
 p {
   color: black;
+}
+.home-container {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+  gap: 50px;
+  text-align: center;
+}
+.button-row {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 33%;
 }
 </style>

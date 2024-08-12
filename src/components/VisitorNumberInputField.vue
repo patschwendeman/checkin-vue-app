663a1139d24eaaf6c54307c98fa223ff9c85f063
Patrick Schwendemann <patrick.schwendemann@bamero.de>
2024-08-12 18:59:34 +0200

    Add visitor number view and component

---
+++ src/components/VisitorNumberInputField.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import CustomButton from './CustomButton.vue'
+</script>
+
+<template>
+  <div class="VisitorNumberInputField">
+    <input type="text" />
+    <nav>
+      <CustomButton text="CHECK IN" backgroundColor="black" color="white" route="/data" />
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+.VisitorNumberInputField {
+  width: 100%;
+  height: 200px;
+  border: solid black 1px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 33px;
+  gap: 33px;
+}
+
+input {
+  width: 100%;
+  height: 100px;
+  border: none;
+}
+</style>
---
+++ src/views/VisitorNumberView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import MainHeader from '../components/MainHeader.vue'
+import VisitorNumberInputField from '../components/VisitorNumberInputField.vue'
+</script>
+
+<template>
+  <MainHeader headline="Checki-In" backgroundColor="grey" />
+  <div class="content">
+    <p>Geben Sie ihre Buchnummer, Ihre E-Mail Adresse oder Ihren Nachnamen ein</p>
+    <VisitorNumberInputField />
+  </div>
+</template>
+
+<style></style>

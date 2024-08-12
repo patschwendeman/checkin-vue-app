592248a46a201e00f098cfcbd08435b3090c215b
Patrick Schwendemann <patrick.schwendemann@bamero.de>
2024-08-12 19:27:40 +0200

    Add shadow and font style

--- src/components/VisitorNumberInputField.vue
+++ src/components/VisitorNumberInputField.vue
@@ -21,11 +21,15 @@ import CustomButton from './CustomButton.vue'
   justify-content: space-between;
   padding: 33px;
   gap: 33px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.2);
 }
 
 input {
   width: 100%;
   height: 100px;
   border: none;
+  font-size: 50px;
+  font-weight: 500;
+  line-height: 58px;
 }
 </style>

acde9bbc3b2b59b9863055cd0e33189653c4a57b
Patrick Schwendemann <patrick.schwendemann@bamero.de>
2024-08-22 18:25:38 +0200

    Add footer bar my data view

--- src/views/MyDataView.vue
+++ src/views/MyDataView.vue
@@ -1,10 +1,26 @@
+<script setup lang="ts">
+import MainHeader from '../components/MainHeader.vue'
+import FooterBar from '../components/FooterBar.vue'
+import CustomButton from '../components/CustomButton.vue'
+</script>
+
 <template>
-    <div class="content">
-      <h1>This is my data page</h1>
-    </div>
-  </template>
-  
-  <style>
-  
-  </style>
-  
+  <MainHeader headline="Checki-In" backgroundColor="#e9eaeb" />
+  <div class="content">
+    <h1>This is my data page</h1>
+  </div>
+  <FooterBar />
+</template>
+
+<style scoped>
+.footerBar {
+  display: flex;
+  width: 100%;
+  height: 136px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  color: white;
+}
+</style>

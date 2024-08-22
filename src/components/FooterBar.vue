88da53c102a8a7c57972f430d01e51b6d79fd53a
Patrick Schwendemann <patrick.schwendemann@bamero.de>
2024-08-22 18:23:46 +0200

    Add footer bar component

---
+++ src/components/FooterBar.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import CustomButton from './CustomButton.vue'
+</script>
+
+<template>
+  <nav class="footerBar">
+    <CustomButton text="ZURÜCK" backgroundColor="white" color="black" route="/auth" />
+    <CustomButton text="WEITER" backgroundColor="black" color="white" route="/auth" />
+  </nav>
+</template>
+
+<style scoped>
+.footerBar {
+  display: flex;
+  width: 100%;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  justify-content: space-between;
+  align-items: center;
+  padding: 33px;
+}
+</style>
